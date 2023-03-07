e3950ee656b62fe37fdeddd37209df62b3637a31
Blanche Haddad <blancherachel@gmail.com>
2023-03-07 10:22:27 -0700

    feat: Add tests for second bullet point, all pass

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -12,5 +12,10 @@ describe('empty spec', () => {
     cy.get(':nth-child(2) > a.url-link').contains("http://localhost:3001/useshorturl/2").should("be.visible");
     cy.get(':nth-child(3) > h3.url-link').contains("coffee").should("be.visible");
     cy.get(':nth-child(3) > a.url-link').contains("http://localhost:3001/useshorturl/3").should("be.visible");
-  })
+  });
+  it("Should display a form and inputs", () => {
+    cy.get('[placeholder="Title..."]').should("be.visible");
+    cy.get('[placeholder="URL to Shorten..."]').should("be.visible");
+    cy.get("button").contains("Shorten Please!").should("be.visible");
+  });
 })
