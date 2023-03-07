18b9a15a38a1327386340ccc263edfe0c134dd9e
Blanche Haddad <blancherachel@gmail.com>
2023-03-07 10:36:28 -0700

    fix: Remove consolelogs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,9 +15,7 @@ export class App extends Component {
 
   componentDidMount() {
     getUrls()
-      .then(data => {this.setState({urls: data.urls})
-      console.log(this.state.urls)
-    })
+      .then(data => this.setState({urls: data.urls}))
   }
 
   grabResponse = (res) => {
